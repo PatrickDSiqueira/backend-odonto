1c554c70f429bfa1c4e5eab8bb9e343850a116d5
Patrick Siqueira <psique.estuda@gmail.com>
2023-10-22 10:37:00 -0300

    Estrutura do Projeto

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const consign = require('consign');
+
+const app = express();
+
+const cors = require('cors');
+
+app.use(express.json())
+app.use(cors())
+consign()
+    .include('src/routes')
+    .then('src/repositories')
+    .then('src/controllers')
+    .into(app);
+
+app.listen(3003, function(){
+    console.log('APP rodando na porta 3003');
+});
